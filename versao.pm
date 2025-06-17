6ff61fbf98e634de16b48a1d64383b22f0d9cba3
pompz-bit <azakura@hotmail.co.jp>
2025-06-17 11:10:15 +0100

    refactor: update version

--- versao.pm
+++ versao.pm
@@ -6,14 +6,16 @@ automacro versao {
     call {
         [
         log =================================================================
-        log   EventMacrosBR / up_1_ao_99_todas_classes-bro
-        log   Versão: <versao>
-        log   https://github.com/eventMacrosBR/up_1_ao_99_todas_classes-bro
-        log   Em caso de duvidas digite:
+        log   Baseado no original: https://github.com/eventMacrosBR/up_1_ao_99_todas_classes-bro
+        log   Fork mantido em: https://github.com/pompz-bit/up_1_ao_99_todas_classes-bro
+        log   Versão do fork: <versao> (pompz-bit)
+        log   Contribua com um café: https://ko-fi.com/pompzbit
+        log   É sempre necessário ter mais pessoas para ajudar a manter o projeto! Sinta-se a vontade para contribuir com código ou sugerir melhorias.
+        log   Em caso de dúvidas digite:
         log   eventMacro ajuda
         log =================================================================
         ]
         do conf -f versao_eventmacro_up_todas_as_classes_bro <versao>
     }
 }
- 
+
